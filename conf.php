61ccf96f965c11f5df192e372016a142ed05c419
Anna Karutina <anna.karutina@khk.ee>
2018-01-18 09:42:06 +0200

    Konfiguratsiooni määratud abikonstandid

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 18.01.2018
+ * Time: 9:34
+ */
+// konfiguratsiooni fail
+
+// loome vajalikud abikonstandid
+define('MODEL_DIR', 'model/');
+define('VIEWS_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
