75509a958f284c91f244773537b6170c34732ceb
Anna Karutina <anna.karutina@khk.ee>
2018-01-19 14:48:31 +0200

    Reaalsed väärtused on määratud

--- index.php
+++ index.php
@@ -10,7 +10,14 @@ require_once 'conf.php';
 
 // loome peamalli objekti
 $mainTmpl = new template('main');
-
+// reaalväärtuste määramine
+$mainTmpl->set('site_lang', 'et');
+$mainTmpl->set('site_title', 'PV');
+$mainTmpl->set('user', 'Kasutaja');
+$mainTmpl->set('title', 'Pealkiri');
+$mainTmpl->set('lang_bar', 'Keeleriba');
+$mainTmpl->set('menu', 'Lehe menüü');
+$mainTmpl->set('content', 'Lehe sisu');
 // väljastame objekti sisu test kujul
 echo '<pre>';
 print_r($mainTmpl);
