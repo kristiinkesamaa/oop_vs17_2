748b0d27016e982a0f1acc75f52e518515d5517c
Anna Karutina <anna.karutina@khk.ee>
2018-01-23 14:24:13 +0200

    Ühe menüü elemendi lisamine on kirjeldatud

--- menu.php
+++ menu.php
@@ -6,4 +6,18 @@
  * Time: 14:13
  */
 
-$mainTmpl->set('menu', 'Lehe menüü');
+// loome menüü ehitamiseks vajalikud objektid
+$menuTmpl = new template('menu.menu'); // menüü mall
+$itemTmpl = new template('menu.item'); // menüü elemendi mall
+
+// loome üks menüü element nimega esimene
+$itemTmpl->set('name', 'esimene');
+// lisame antud element menüüsse
+$menuItem = $itemTmpl->parse(); // string, mis sisaldab ühe nimekirja elemendi lingiga
+$menuTmpl->set('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>
+
+// ehitame valmis menüü
+$menu = $menuTmpl->parse();
+
+// lisame valmis menüü lehele
+$mainTmpl->set('menu', $menu);
