91328059f26c2fb925bff3e7b7f4c92f3b9289dd
Anna Karutina <anna.karutina@khk.ee>
2018-01-24 09:34:52 +0200

    Põhilingi loomine on lisatud

--- model/linkobject.php
+++ model/linkobject.php
@@ -8,6 +8,18 @@
 
 class linkobject extends http
 {
+    // klassi muutujad/väljad
+    var $baseLink = false; // põhilink
+    var $protocol = 'http://'; // HTTP protokoll
+    var $eq = '='; // =
+    var $delim = '&amp;'; // &
 
-
+    /**
+     * linkobject constructor.
+     */
+    public function __construct(){
+        // kõigepealt defineerime vajalikud eelandmed
+        parent::__construct();
+        $this->baseLink = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }
 }
