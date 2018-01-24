986e0cecee87106fbc57e589d721256eec8a5b56
Anna Karutina <anna.karutina@khk.ee>
2018-01-24 09:18:29 +0200

    Konfiguratsioon on täiendatud lingi loomise objektiga

--- conf.php
+++ conf.php
@@ -15,6 +15,7 @@ define('CONTROL_DIR', 'controllers/');
 // nõuame vajalikke failide olemasolu
 require_once MODEL_DIR.'template.php'; // html vaade failide töötlus
 require_once MODEL_DIR.'http.php'; // HTTP töötlus klass
+require_once MODEL_DIR.'linkobject.php'; // Lingi töötluse klass
 
 // loome vajalikud objektid, mis on pidevalt tööl
-$http = new http();
+$http = new linkobject();
