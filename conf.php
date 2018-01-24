5b01c1c35e3a733ce134fbb761a10554251903c5
Anna Karutina <anna.karutina@khk.ee>
2018-01-24 10:13:48 +0200

    Konfiguratsioon on täiendatud abifunktsiooniga

--- conf.php
+++ conf.php
@@ -11,6 +11,10 @@
 define('MODEL_DIR', 'model/');
 define('VIEWS_DIR', 'views/');
 define('CONTROL_DIR', 'controllers/');
+define('LIB_DIR', 'lib/');
+
+// nõuame abifunktisoonide olemasolu
+require_once LIB_DIR.'utils.php';
 
 // nõuame vajalikke failide olemasolu
 require_once MODEL_DIR.'template.php'; // html vaade failide töötlus
