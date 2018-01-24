fc49bd54442a5bd3a80bfa84af1c1ad88d40717c
Anna Karutina <anna.karutina@khk.ee>
2018-01-24 11:01:40 +0200

    Menüü on täiendatud linkidega

--- menu.php
+++ menu.php
@@ -11,13 +11,22 @@ $menuTmpl = new template('menu.menu'); // menüü mall
 $itemTmpl = new template('menu.item'); // menüü elemendi mall
 
 // loome üks menüü element nimega esimene
+// määrame menüüs väljastava elemendi nime
 $itemTmpl->set('name', 'esimene');
+// määrata menüüs väljastava elemendiga seotud link
+// http://anna.ikt.khk.ee/oop_vs17_2/index.php?control=esimene
+$link = $http->getLink(array('control'=>'esimene'));
+$itemTmpl->set('link', $link);
 // lisame antud element menüüsse
 $menuItem = $itemTmpl->parse(); // string, mis sisaldab ühe nimekirja elemendi lingiga
 $menuTmpl->add('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>
 
 // loome veel üks menüü element nimega teine
 $itemTmpl->set('name', 'teine');
+// määrata menüüs väljastava elemendiga seotud link
+// http://anna.ikt.khk.ee/oop_vs17_2/index.php?control=esimene
+$link = $http->getLink(array('control'=>'teine'));
+$itemTmpl->set('link', $link);
 // lisame antud element menüüsse
 $menuItem = $itemTmpl->parse(); // string, mis sisaldab ühe nimekirja elemendi lingiga
 $menuTmpl->add('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>
