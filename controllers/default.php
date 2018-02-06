f7528d33f66ca90cb078fd6e3a2b8734746b552c
kristiin.kesamaa <kristiinkesamaa@khk.ee>
2018-02-06 13:09:27 +0200

    koostatud esileht, mis ei muutu

--- controllers/default.php
+++ controllers/default.php
@@ -5,4 +5,19 @@
  * Date: 31.01.2018
  * Time: 11:01
  */
-$mainTmpl->set('content', 'Vaikimisi sisu');
+
+$page_id = (int)$http->get('page_id'); // lehe id
+// koostame päringu contenti jaoks
+$sql = 'SELECT * FROM content WHERE content_id='.fixDB($page_id);
+// selecti päringuga on getdata päring
+// küsime andmed andmebaasist
+$result = $db->getData($sql);
+if($result == false){
+    $sql = 'SELECT * FROM content WHERE is_first_page='.fixDB(1);
+    $result = $db->getData($sql);
+}
+if($result != false){
+    $page = $result[0];
+    $http->set('page_id', $page['content_id']);
+    $mainTmpl->set('content', $page['content']);
+}
