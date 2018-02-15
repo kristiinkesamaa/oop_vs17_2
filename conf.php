5aa29e2ab6266fd239510298846bdb4a2a5e4687
kristiin.kesamaa <kristiinkesamaa@khk.ee>
2018-02-15 10:14:21 +0200

    lisasime erinevad kasutajate rollid

--- conf.php
+++ conf.php
@@ -18,17 +18,18 @@ define('DEFAULT_CONTROL', 'default');
 // nõuame abifunktisoonide olemasolu
 require_once LIB_DIR.'utils.php';
 
+// lisame rakendusel kasutatavad kasutajate rollid
+define('ROLE_NONE', 0);
+define('ROLE_USER', 1);
+define('ROLE_ADMIN', 2);
 // nõuame vajalikke failide olemasolu
 require_once MODEL_DIR.'template.php'; // html vaade failide töötlus
 require_once MODEL_DIR.'http.php'; // HTTP töötlus klass
 require_once MODEL_DIR.'linkobject.php'; // Lingi töötluse klass
-
-require_once MODEL_DIR.'mysql.php'; //db töötlus klass
-
-//nõuame vajaliku konfi olemasolu
+require_once MODEL_DIR.'mysql.php'; // DB töötlus klass
+// nõuame vajalikke konfiguratsioonide olemasolu
 require_once 'db_conf.php';
-
 // loome vajalikud objektid, mis on pidevalt tööl
-$http = new linkobject();
+$http = new linkobject(); // http ja lingi objekt
 // andmebaasi objekt
 $db = new mysql(DB_HOST, DB_USER, DB_PASS, DB_NAME);
